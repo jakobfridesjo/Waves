915bb607f05bf85d7c7ac0471f65d5807a0e89cf
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2023-04-04 22:29:29 +0200

    Implementing a bottom navigation bar and fragments for it

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.myapplication
 
 import android.media.AudioAttributes
-import android.media.AudioManager
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Get binding of the main activity*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* Bottom nav bar */
+        val navView: BottomNavigationView = binding.navView
+        //val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        //val appBarConfiguration = AppBarConfiguration(setOf(
+        //    R.id.navigation_notes, R.id.navigation_navigation, R.id.navigation_utilities))
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        //navView.setupWithNavController(navController)
 
         val mediaPlayer = MediaPlayer()
         val audioAttributes = AudioAttributes.Builder()
