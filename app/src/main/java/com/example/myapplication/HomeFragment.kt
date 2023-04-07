861b256c0bd859acf9ec071eeaf1626a80dc1f8f
Jakob Fridesjö <95225480+jakobfridesjo@users.noreply.github.com>
2023-04-07 20:21:44 +0000

    Map (#5)
    * fixes to navbar fragments
    
    * added a map to the homepage
    
    * minor fix to layout before merge
    
    ---------
    
    Co-authored-by: Jakob Fridesjö <tfy17jfo@cs.umu.se>

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentHomeBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapController
+import org.osmdroid.views.overlay.OverlayItem
 
 /**
  * A simple [Fragment] subclass.
@@ -14,6 +21,10 @@ import com.example.myapplication.databinding.FragmentHomeBinding
  */
 class HomeFragment : Fragment() {
 
+    // OpenStreeMap
+    lateinit var mapController: MapController
+    lateinit var overlayItem: ArrayList<OverlayItem>
+    val location = GeoPoint(65.5840799,22.1975568)
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -26,6 +37,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID;
+        binding.mapView.setMultiTouchControls(true)
+        binding.mapView.controller.animateTo(location)
+        binding.mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+        binding.mapView.setUseDataConnection(true)
+        binding.mapView.setTileSource(TileSourceFactory.WIKIMEDIA)
+        mapController = binding.mapView.controller as MapController
+        mapController.setCenter(location)
+        mapController.zoomTo(15)
 
         return binding.root
     }
