63da9347dd32df13e9c05ae431fe7250138cfbda
Jakob Fridesjö <95225480+jakobfridesjo@users.noreply.github.com>
2023-04-07 16:55:02 +0000

    fixes to navbar fragments (#4)
    Co-authored-by: Jakob Fridesjö <tfy17jfo@cs.umu.se>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,28 +5,32 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         /* Get binding of the main activity*/
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         /* Bottom nav bar */
-        //val navView: BottomNavigationView = binding.navView
-        //val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        //val appBarConfiguration = AppBarConfiguration(setOf(
-        //    R.id.navigation_notes, R.id.navigation_navigation, R.id.navigation_utilities))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-        //navView.setupWithNavController(navController)
+        // menu should be considered as top level destinations
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_search, R.id.navigation_favorites))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         val mediaPlayer = MediaPlayer()
         val audioAttributes = AudioAttributes.Builder()
