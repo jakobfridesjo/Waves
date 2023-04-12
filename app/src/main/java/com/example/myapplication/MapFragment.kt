3f77d1f93e6ff496354b0c5ed02038e8906014d0
Jakob Fridesjö <95225480+jakobfridesjo@users.noreply.github.com>
2023-04-12 08:58:19 +0000

    Added markers to the map (#13)
    Co-authored-by: Jakob Fridesjö <tfy17jfo@cs.umu.se>

--- app/src/main/java/com/example/myapplication/MapFragment.kt
+++ app/src/main/java/com/example/myapplication/MapFragment.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.LayoutInflater
@@ -15,7 +19,7 @@ import org.osmdroid.util.MapTileIndex
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapController
 import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.OverlayItem
+import org.osmdroid.views.overlay.Marker
 
 
 /**
@@ -27,7 +31,7 @@ class MapFragment : Fragment() {
 
     // OpenStreeMap
     lateinit var mapController: MapController
-    lateinit var overlayItem: ArrayList<OverlayItem>
+    lateinit var geoPoints: List<GeoPoint>
     private val location = GeoPoint(65.5840799,22.1975568)
     private var _binding: FragmentMapBinding? = null
 
@@ -50,6 +54,29 @@ class MapFragment : Fragment() {
         binding.mapView.controller.animateTo(location)
         binding.mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
         binding.mapView.setUseDataConnection(true)
+
+        // Set some random locations for testing point drawing
+        geoPoints = listOf(
+            GeoPoint(65.584160,22.154751),
+            GeoPoint(52.520008,13.404954),
+            GeoPoint(39.904202,116.407394),
+            GeoPoint(35.689487,139.691711),
+            GeoPoint(-18.379554,46.628136),
+            GeoPoint(-4.664757,-59.786157)
+        )
+
+        // Setup markers
+        val markers = geoPoints.map {
+            val marker = Marker(binding.mapView)
+            marker.position = it
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            marker.icon = generateCircle(20, Color.GREEN)
+            marker
+        }
+
+        // Add markers to mapView
+        binding.mapView.overlays.addAll(markers)
+        binding.mapView.invalidate()
 
         // Disable repetition and set scrolling limits so that the map only tiles horizontally
         binding.mapView.isVerticalMapRepetitionEnabled = false
@@ -88,5 +115,26 @@ class MapFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /*
+     * Generates a drawable circle bitmap
+     */
+    private fun generateCircle(diameter: Int, color: Int): BitmapDrawable {
+
+        val circleBMP = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(circleBMP)
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = color
+        paint.style = Paint.Style.FILL
+
+        val centerX = diameter / 2f
+        val centerY = diameter / 2f
+        val radius = diameter / 2f
+
+        canvas.drawCircle(centerX, centerY, radius, paint)
+
+        return BitmapDrawable(resources, circleBMP)
     }
 }
